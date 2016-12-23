bdb2f8ef270cae94a6227c42a0585f0a0d93dd41
velopert <public.velopert@gmail.com>
2016-12-23 20:23:26 +0900

    9

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import './Animation.css';
+import Promise from 'promise-polyfill'; 
+ 
+// To add to window
+if (!window.Promise) {
+  window.Promise = Promise;
+}
  
 ReactDOM.render(
   <App />,
