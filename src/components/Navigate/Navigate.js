5060b6f293724c4be1e93d8b6b24b5acbf86d49d
velopert <public.velopert@gmail.com>
2016-12-23 19:18:08 +0900

    6.3

--- src/components/Navigate/Navigate.js
+++ src/components/Navigate/Navigate.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {Button} from 'semantic-ui-react';
 import './Navigate.css'
  
-const Navigate = () => (
+const Navigate = ({onClick, postId, disabled}) => (
     <div className="Navigate">
         <Button
             color="teal"
             content="Previous"
             icon="left arrow"
             labelPosition="left"
+            onClick={
+                () => onClick('PREV')
+            }
+            disabled={disabled}
         />
         <div className="Navigate-page-num">
-            1
+            {postId}
         </div>
         <Button
             color="teal"
@@ -19,6 +23,10 @@ const Navigate = () => (
             icon="right arrow"
             labelPosition="right"
             className="Navigate-right-button"
+            onClick={
+                () => onClick('NEXT')
+            }
+            disabled={disabled}
         /> 
     </div>
 );
