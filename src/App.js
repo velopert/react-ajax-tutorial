e83603f12885c27d159f2c42b3ce5b4eea847ace
velopert <public.velopert@gmail.com>
2016-12-23 18:51:15 +0900

    3.6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import {PostContainer} from './containers';
 import { Header } from './components';
  
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header/>
-            </div>
-        );
-    }
+  render() {
+    return (
+        <div>
+            <Header/>
+            <PostContainer/>
+        </div>    
+    );
+  }
 }
  
 export default App;
---
+++ src/containers/PostContainer/PostContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { PostWrapper } from '../../components';
+ 
+class PostContainer extends Component {
+    render() {
+        return (
+            <PostWrapper>
+                Hello, Post
+            </PostWrapper>
+        );
+    }
+}
+ 
+export default PostContainer;
