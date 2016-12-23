fdcd23a3b96167e868655b4466eaab14ea19ec99
velopert <public.velopert@gmail.com>
2016-12-23 19:19:54 +0900

    7.1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import './Animation.css';
  
 ReactDOM.render(
   <App />,
