c53bae0c415276a6b80b7a1f9d43b468f58db243
velopert <public.velopert@gmail.com>
2016-12-23 19:14:04 +0900

    5.5

--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -5,25 +5,70 @@ import * as service from '../../services/post';
  
 class PostContainer extends Component {
  
+    constructor(props) {
+        super();
+        // initializes component state
+        this.state = {
+            postId: 1,
+            fetching: false, // tells whether the request is waiting for response or not
+            post: {
+                title: null,
+                body: null
+            },
+            comments: []
+        };
+    }
+
     componentDidMount() {
         this.fetchPostInfo(1);
     }
+    
 
 
     fetchPostInfo = async (postId) => {
+        
+        this.setState({
+            fetching: true // requesting..
+        });
+ 
+        // wait for two promises
         const info = await Promise.all([
             service.getPost(postId),
             service.getComments(postId)
         ]);
         
-        console.log(info);
+        // Object destructuring Syntax,
+        // takes out required values and create references to them
+        const {title, body} = info[0].data; 
+                                            
+        const comments = info[1].data;
+ 
+        this.setState({
+            postId,
+            post: {
+                title, 
+                body
+            },
+            comments,
+            fetching: false // done!
+        });
+ 
     }
  
     render() {
+        const {postId, fetching, post, comments} = this.state;
+ 
         return (
             <PostWrapper>
-                <Navigate/>
-                <Post/>
+                <Navigate 
+                    postId={postId}
+                    disabled={fetching}
+                />
+                <Post
+                    title={post.title}
+                    body={post.body}
+                    comments={comments}
+                />
             </PostWrapper>
         );
     }
