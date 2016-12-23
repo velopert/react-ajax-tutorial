8885a823d91de864f7b72fd65640764b904aa22b
velopert <public.velopert@gmail.com>
2016-12-23 18:45:51 +0900

    3.4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Header } from './components';
  
 class App extends Component {
     render() {
         return (
             <div>
+                <Header/>
             </div>
         );
     }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+    <div className="Header">
+        POSTS
+    </div>
+)
+
+export default Header;
