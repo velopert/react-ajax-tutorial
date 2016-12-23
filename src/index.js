a43b372955df0d0a9b8efc16ac8c0c33cf6bbc69
velopert <public.velopert@gmail.com>
2016-12-23 18:41:17 +0900

    3.2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-
+ 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
