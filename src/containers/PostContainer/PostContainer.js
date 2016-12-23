c560a29d86fa6a32d47a8215d04a9a957b9b6bb5
velopert <public.velopert@gmail.com>
2016-12-23 19:17:05 +0900

    6.1

--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -54,6 +54,16 @@ class PostContainer extends Component {
         });
  
     }
+
+    handleNavigateClick = (type) => {
+        const postId = this.state.postId;
+ 
+        if(type === 'NEXT') {
+            this.fetchPostInfo(postId+1);
+        } else {
+            this.fetchPostInfo(postId-1);
+        }
+    }
  
     render() {
         const {postId, fetching, post, comments} = this.state;
